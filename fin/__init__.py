1241ab3a72888ba37edc93eb93bc9060582a2813
svalqui <svalqui@acslink.net.au>
2018-03-16 09:26:56 +1100

    fin isaumrkt open start

--- fin/__init__.py
+++ fin/__init__.py
@@ -5,4 +5,11 @@ def IsAuMrkO(self):
 
 #  https://www.asx.com.au/
 
+    import requests
+    from bs4 import BeautifulSoup
+
+    # Collect and parse page
+    page = requests.get('https://www.asx.com.au')
+    soup = BeautifulSoup(page.text, 'html.parser')
+
     return opened
