825820df7f69d90901820477a4e240eea248f304
svalqui <svalqui@unimelb.edu.au>
2019-09-19 16:53:10 +1000

    draft.

---
+++ texpar/fp.py
@@ -0,0 +1,17 @@
+from collections.abc import MutableMapping
+from collections import OrderedDict as _default_dict, ChainMap as _ChainMap
+import functools
+import io
+import itertools
+import os
+import re
+import sys
+import warnings
+
+
+def _read(self, fp):
+
+
+def read_file(self, f):
+
+    self._read(f)
