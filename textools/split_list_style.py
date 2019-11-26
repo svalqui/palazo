a2d1c5de8166c6dabd84c46dc6dc63476becc043
svalqui <svalqui@unimelb.edu.au>
2019-11-26 17:00:53 +1100

    slit_lists, draft.

--- textools/split_list_style.py
+++ textools/split_list_style.py
@@ -5,13 +5,22 @@ def split_list(my_list, indentation_spaces=8, items_per_line=4):
     edited_list = ''
     if isinstance(my_list, tuple):
         line = " " * indentation_spaces + "("
+        new_line = True
         for counter, item in enumerate(my_list):
             if (counter + 1) % items_per_line != 0:
                 line += '"' + item + '"' + ', '
+                new_line = False
             else:
                 line += '\n'
                 edited_list += line
                 line = " " * indentation_spaces + '"' + item + '"' + ', '
+                new_line = True
+
+
+
+
+
+
 
 
 
