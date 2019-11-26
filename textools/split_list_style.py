b9d255f135bc128111cca25e12e62fbac5a2866a
svalqui <svalqui@acslink.net.au>
2019-11-27 10:42:13 +1100

    split list style.

--- textools/split_list_style.py
+++ textools/split_list_style.py
@@ -1,18 +1,45 @@
 # Copyright 2019 by Sergio Valqui. All rights reserved.
 
+import sys
 
-def split_list(my_list, indentation_spaces=8, items_per_line=4):
+
+def split_list(my_list, spaces=8, items_per_line=4):
     edited_list = ''
-    if isinstance(my_list, tuple):
-        line = " " * indentation_spaces + "("
-        for counter, item in enumerate(my_list):
-            if (counter + 1) % items_per_line != 0:
-                line += '"' + item + '"' + ', '
-            else:
-                line += '\n'
-                edited_list += line
-                line = " " * indentation_spaces + '"' + item + '"' + ', '
+    if isinstance(my_list, tuple) or isinstance(my_list,list):
+        if isinstance(my_list,tuple):
+            start_char = "("
+            end_char = ")"
+            print('This is a tuple')
+        else:
+            start_char = "("
+            end_char = ")"
+            print('This is a list')
 
+        line = " " * spaces + start_char
+        for counter, item in enumerate(my_list):
+            if counter % items_per_line == 0:
+                if counter == 0:
+                    line += '"' + item + str(counter) + '"' + ', '
+                else:
+                    line += '\n'
+                    edited_list += line
+                    line = " " * spaces + '"' + item + str(counter) + '"' + ', '
+            else:
+                line += '"' + item + str(counter) + '"' + ', '
+        line += '\n'
+        edited_list += line
+        edited_list += ' ' * spaces + end_char
+
+    else:
+        raise ValueError('Only for tuple and list, not for ', type(my_list))
+
+    print(edited_list)
+
+    return edited_list
+
+
+if __name__ == '__main__':
+    sys.exit(main())
 
 
 
