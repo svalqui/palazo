ff18667bda267370d484f3192cf67374104cc2d6
Sergio Valqui <svalqui@users.noreply.github.com>
2023-04-24 13:17:14 +1000

    added to bash examples

--- bash/sv-bash02-files.sh
+++ bash/sv-bash02-files.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 #
-#
 # References
 # https://askubuntu.com/questions/605411/difference-between-and-in-bash
 # https://stackoverflow.com/questions/2437452/how-to-get-the-list-of-files-in-a-directory-in-a-shell-script
+# https://stackoverflow.com/questions/26575507/sort-list-of-files-by-date-in-bash
+#
 #
 echo  "List files in directory"
+#
+# Using a variable 
 dir=/etc/sysstat/*
 
 for entry in $dir
@@ -22,8 +25,23 @@ echo "$(stat /etc/issue)"
 echo
 echo "file mod date: `stat -c%y /etc/issue`"
 echo
-
+#
+# Using find
 for FILE in `find /dev/tty2* -type c`
 do
   echo "$FILE"
 done
+echo
+#
+# Using a directory and wildcard
+for entry in /dev/tty2*
+do 
+  echo "$entry"
+done
+echo
+#
+# Sort by date
+for f in "$HOME/*" 
+do
+  stat -c '%Y %n' $f
+done | sort -n | cut -d ' ' -f2
--- bash/sv-bash03-dates.sh
+++ bash/sv-bash03-dates.sh
@@ -6,9 +6,11 @@
 # https://stackoverflow.com/questions/9008824/how-do-i-get-the-difference-between-two-dates-under-bash
 # https://phoenixnap.com/kb/bash-math
 #
+# Date today
 echo "The date today, default: "
 echo "    $(date)"
 echo
+# Date today in a given format
 echo 'The date today, format +"%m-%d-%Y"'
 echo "    $(date +"%m-%d-%Y")"
 echo
@@ -17,8 +19,10 @@ echo "Show file stats "
 echo "file /etc/issue"
 echo "$(stat /etc/issue)"
 echo
+# File last modified date
 echo "file mod date: `stat -c%y /etc/issue`"
 echo
+# Days difference between file's mod date and today
 my_file_date_sec=`stat -c%Y /etc/issue`
 echo "file mod date, in seconds since epoch $my_file_date_sec"
 echo
@@ -29,3 +33,30 @@ echo
 let my_diff=($my_date_sec - $my_file_date_sec)/86400
 echo "$my_diff days since file '/etc/issue' modification date and today"
 echo
+#
+# Sort by date
+for f in `find $HOME/ -maxdepth 1 -type f`
+do
+  stat -c '%Y %n' $f
+#done | sort -n | cut -d ' ' -f2
+done | sort -n
+#
+# Files older than a week
+for f in `find $HOME/ -maxdepth 1 -mtime -7 -type f`
+do
+  echo "file $f newer than a week"
+#done | sort -n | cut -d ' ' -f2
+done | sort -n
+echo
+#
+# Pretent delete older files
+# deleting older files of a given name
+while (ls $HOME/python3* | wc -l > 2 ) # If there are more than 2
+do
+  echo "$(ls -t $HOME/python3* | tail -1)" # Delete the older first
+  break
+done
+
+
+
+
