6ae62d6bb5e71e06bc4a5682806fc52d0c269ad1
Sergio Valqui <svalqui@users.noreply.github.com>
2023-08-21 14:38:29 +1000

    added more detail to migration

--- ostack/migrate.py
+++ ostack/migrate.py
@@ -11,6 +11,7 @@
 # live_migrate(host=None, block_migration=None)
 
 import os
+import random
 import sys
 import time
 
@@ -18,6 +19,15 @@ from keystoneauth1.identity import v3
 from keystoneauth1 import session
 
 from novaclient import client as nov_cli
+
+
+def print_structure(my_obj, geta=True):
+    """Prints attributes of an Obj."""
+    for att in dir(my_obj):
+        if geta:
+            print(att, getattr(my_obj, att), type(getattr(my_obj, att)).__name__)
+        else:
+            print(att, type(getattr(my_obj, att)).__name__)
 
 def os_auth_env_sess():
     """Authenticates using keystone, using environmental variables. returns a session"""
@@ -30,6 +40,49 @@ def os_auth_env_sess():
 
     return os_session
 
+def server_list_per_host(nv_client, my_host):
+    """List servers on the given host."""
+    svrs = nv_client.servers.list(search_opts={'host': my_host, 'all_tenants': 1})
+
+    for counter, svr in enumerate(svrs):
+        av_zone = getattr(svr, "OS-EXT-AZ:availability_zone")
+        # locked = getattr(svr, "locked") # it seems it can't be queried
+        # print_structure(svr)
+        print(counter + 1, svr.id, svr.name, svr.status, av_zone,
+              svr.addresses)
+              # svr.addresses, svr.security_groups, svr.metadata)
+        #inst_vols = getattr(svr, "os-extended-volumes:volumes_attached")
+        #if len(inst_vols) > 0:
+        #    for inst_vol in inst_vols:
+        #        print("    vol_att_id:", inst_vol['id'])
+    # print_structure(svrs[0])
+    return svrs
+
+def put_in_main_agg(nv_client, my_host):
+    """."""
+    all_aggre = nv_client.aggregates.list()
+    current_aggre= []
+    current_aggre_names = []
+    for agg in all_aggre:
+        if "m-" in agg.name:
+            # aggre = nv_client.aggregates.get(agg_name)
+            if my_host in agg.hosts:
+                current_aggre.append(agg)
+                current_aggre_names.append(agg.name)
+    # put in main
+    for a in current_aggre:
+        print('Current Aggregates :', a.id, a.name)
+    if 'm1' not in current_aggre_names:
+
+        print('adding :', my_host, ' to: m1')
+        nv_client.aggregates.add_host(<aggregte-maint-id>, my_host)
+
+    # remove of all others
+        for current_a in current_aggre:
+            print('removing :', my_host, ' from: ', current_a.id, current_a.name )
+            nv_client.aggregates.remove_host(current_a.id, my_host)
+    return
+
 def main():
     """ CLI implementation temporal for fast trial while developing
     """
@@ -40,23 +93,27 @@ def main():
 
     nv_client = nov_cli.Client(version=2, session=my_session)
 
-    # List servers on host
-
     # input source host here
-    source_host = ''
+    source_host = input('which host to evacuate: ')
     # input target host here
-    target_host = ''
+    #target_hosts = input('into which hosts(,): ')
+    target_hosts = [ 'qh2-rcc03', 'qh2-rcc04', 'qh2-rcc05' ]
+
+    # List servers on host
+    servers = server_list_per_host(nv_client, source_host)
+
 
     # check source host is on maintenance aggregate, if not
     # move the source host to the maintenance aggregate, add to maintenance remove from current
+    put_in_main_agg(nv_client, source_host)
 
-    #
-    #
-    servers = nv_client.servers.list(search_opts={'host': source_host, 'all_tenants': 1})
-    for server in servers:
+    n_servers = len(servers)
+    for idx, server in enumerate(servers):
         # line = " " + server.id + ", " + server.name + ", " + server.status + ", "
         # print(line)
-        print ("---")
+
+        target_host = random.choice(target_hosts)
+        print ("---v ", idx+1 ," of ", n_servers)
         if server.status == "ACTIVE": # If active migrate
             print ("Migrating ", server.id, server.name, " from: ", source_host, " to ", target_host)
             if target_host != '':
@@ -112,8 +169,8 @@ def main():
         my_server = nv_client.servers.get(server.id)
         print("Migrated ", my_server.id, my_server.name, my_server.status, getattr(my_server, "OS-EXT-SRV-ATTR:host") )
 
-
-
+        print('Stopping 5 Secs')
+        time.sleep(5)
 
 if __name__ == '__main__':
     sys.exit(main())
