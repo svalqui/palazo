f187ec0e060f3d93262ce57238273b1cf78c0548
Sergio Valqui <svalqui@users.noreply.github.com>
2025-02-05 17:18:17 +1100

    Tenable query test

---
+++ restapi/tenableq.py
@@ -0,0 +1,37 @@
+import ipaddress
+import sys
+from tenable.io import TenableIO
+
+def main():
+    tio_access_key = input("TIO_ACCESS_KEY")
+    tio_secret_key = input("TIO_SECRET_KEY")
+    cidr = input("CIDR")
+
+    tenable_vendor = ""
+    tenable_product = ""
+    tenable_version = ""
+
+    tio = TenableIO(
+        tio_access_key,
+        tio_secret_key,
+        vendor=tenable_vendor,
+        product=tenable_product,
+        build=tenable_version,
+    )
+
+    cidr_obj = ipaddress.ip_network(cidr)
+
+    print(cidr_obj)
+
+#    results = tio.exports.vulns(plugin_id=plugin_ids, cidr_range=config.TARGET_CIDR)
+    results = tio.exports.vulns(cidr_range=cidr)
+
+    for r in results:
+        print(r)
+        print("-----------------")
+        print()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
