12911dc56f4abf3d64eff1281d1f0a5e91fcd6b7
Sergio Valqui <svalqui@users.noreply.github.com>
2025-05-08 11:22:30 +1000

    testing maas rest api

--- restapi/maas.py
+++ restapi/maas.py
@@ -9,6 +9,47 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 import maas.client
 
 from tools.util_obj import print_structure_det
+
+
+def machine_list_all(client):
+    for m in client.machines.list():
+        print(m.fqdn,
+              # m.hostname,
+              m.distro_series,
+              m.power_type,
+              m.cpus,
+              m.memory,
+              # m.zone,
+              # m.pool,
+              m.power_state,
+              m.ip_addresses,
+              )
+
+def machine_list_ipmi(client):
+    for m in client.machines.list():
+        if m.power_type == 'ipmi':
+            print(m.fqdn,
+                  m.distro_series,
+                  m.power_type,
+                  m.cpus,
+                  m.memory,
+                  m.power_state,
+                  m.ip_addresses,
+                  )
+
+def machine_list_virsh(client):
+    for m in client.machines.list():
+        if m.power_type == 'virsh':
+            print(m.fqdn,
+                  m.distro_series,
+                  m.power_type,
+                  m.cpus,
+                  m.memory,
+                  m.power_state,
+                  m.ip_addresses,
+                  )
+
+
 def main():
     file_conf_dir = pathlib.Path(__file__).absolute().parents[2]
     print('file_conf_dir', file_conf_dir)
@@ -22,6 +63,24 @@ def main():
     maas_apikey = config['maas']['apikey']
 
     client = maas.client.connect( maas_url, apikey=maas_apikey)
+
+
+    print(" select what to do: \n"
+          "(a) list all machines \n"
+          "(ipmi) list all ipimi machines \n"
+          "(virsh) list all virtual machine \n")
+
+    option = input("select your option: ")
+
+    if option == 'a':
+        machine_list_all(client)
+    elif option == 'ipmi':
+        machine_list_ipmi(client)
+    elif option == 'virsh':
+        machine_list_virsh(client)
+
+    exit()
+
 
     for m in client.machines.list():
         # print(dir(m))
@@ -57,30 +116,11 @@ def main():
                     # print(type(m.status))
 
 
-
-
 # Get a reference to self.
     myself = client.users.whoami()
 
-
-
     print(myself)
     assert myself.is_admin, "%s is not an admin" % myself.username
-
-    for m in client.machines.list():
-        print(repr(m))
-        print("To print structure detailed")
-        print()
-        print(m.hostname)
-        print(m.zone)
-        print(m.zone.name)
-        print(m.zone.virtualblockdevice_set)
-        for i in m.zone:
-            print (i)
-        #print_structure_det(m, True)
-
-        break
-
 
 
 if __name__ == '__main__':
