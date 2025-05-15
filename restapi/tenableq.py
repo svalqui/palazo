1e11107cceecf8a54a35bcc524825bd8448ca9fd
Sergio Valqui <svalqui@users.noreply.github.com>
2025-05-15 22:32:18 +1000

    tenable show some vulnerabilities details

--- restapi/tenableq.py
+++ restapi/tenableq.py
@@ -3,9 +3,9 @@ import sys
 from tenable.io import TenableIO
 
 def main():
-    tio_access_key = input("TIO_ACCESS_KEY")
-    tio_secret_key = input("TIO_SECRET_KEY")
-    cidr = input("CIDR")
+    tio_access_key = input("TIO_ACCESS_KEY: ")
+    tio_secret_key = input("TIO_SECRET_KEY: ")
+    cidr = input("CIDR: ")
 
     tenable_vendor = ""
     tenable_product = ""
@@ -25,9 +25,18 @@ def main():
 
 #    results = tio.exports.vulns(plugin_id=plugin_ids, cidr_range=config.TARGET_CIDR)
     # https://pytenable.readthedocs.io/en/1.4.7/api/io/exports.html
-    results = tio.exports.vulns(cidr_range=cidr)
+    # results = tio.exports.vulns(plugin_id=[149334,], cidr_range=cidr)
+    results = tio.exports.vulns(severity=["critical", ], cidr_range=cidr)
 
     for r in results:
+        if 'fqdn' in r['asset'].keys():
+            print('fqdn: ', r['asset']['fqdn'])
+        print('hostname: ', r['asset']['hostname'])
+        print('ipv4: ', r['asset']['ipv4'])
+        print('last_scan: ', r['asset']['last_scan_target'])
+        print('port: ', r['port'])
+        print('plugin name: ', r['plugin']['name'])
+        print('plugin id: ', r['plugin']['bid'])
         print(r)
         print("-----------------")
         print()
