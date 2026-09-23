5f7fd20e7939564e268eb3fdbaee65835512e838
Sergio Valqui <svalqui@users.noreply.github.com>
2026-09-23 20:35:38 +1000

    adding cluster name

--- ostack/vm-storage-report.py
+++ ostack/vm-storage-report.py
@@ -11,8 +11,10 @@ def main():
     """ OS VM Report
     """
 
+    print("Connecting...")
     os_conn = openstack.connect(cloud='envvars')
 
+    print("Getting Hypervisors...")
     hypervisors = os_conn.compute.hypervisors()
  #   projects = list(os_conn.identity.projects(limit=10000))
  #   print("projects", len(projects))
@@ -35,6 +37,9 @@ def main():
                 got_image = False
                 got_volume = False
 
+                #print(vm)
+                #print(dir(vm))
+
                 if vm.project_id in projects_dict:
                     prj_name = projects_dict[vm.project_id].name
                 else:
@@ -42,20 +47,55 @@ def main():
                     projects_dict[vm.project_id] = project
                     prj_name = project.name
 
-                print(f"    {vm.id}, {vm.name[0:20]}, {prj_name[0:20]}, Status: {vm.status}")
+                locked_compose = ""
+                if vm.is_locked:
+                    locked_compose = "LOCKED " + vm.locked_reason
+
+                print(f"    {vm.id}, {vm.name[0:20]}, {prj_name[0:20]}, Status: {vm.status}, {locked_compose}")
                 if vm.image:
                     print(f"        Image ID: {vm.image.id}, Name: {vm.image.name}")
                     got_image = True
-                if vm.attached_volumes:
+                if vm.attached_volumes :
                     # details are in the volume attachments
                     attachments = os_conn.compute.volume_attachments(vm.id)
-
                     for att in attachments:
                         #print(att)
                         # print(dir(att))
                         # print("    Volume: ", att['device'], att['volume_id'], att['attachment_id'])
-                        print("        ", att.device, att.server_id, att.volume_id, att.attachment_id, att.bdm_id)
+                        volume = os_conn.get_volume(att.volume_id)
+
+                        line = f"        {att.device}, {att.server_id}, {att.volume_id}, {att.attachment_id}, {att.bdm_id}, {volume.cluster_name}"
+                        print(line)
+
+
+                    #print("VM id: ", vm.id)
+                    #print("Attached Volumes:")
+                    #print(vm.attached_volumes)
+                    # details are in the volume attachments
+                    # or in the volume attachments=[{
+                    #for att_vol in vm.attached_volumes:
+                    #    #print(att_vol)
+                    #    #print(dir(att_vol))
+                    #    #print("    ", att_vol.id, att_vol.volume_id)
+                    #    vol = os_conn.block_storage.get_volume(att_vol.id)
+                    #    #print("---")
+                    #    #print(vol)
+                    #    #print(dir(vol))
+                    #    print(vol.attachments)
+                    #    #print(vol.attachments[0])
+                    #    #print("====")
+                    #    # print("    Volume: ", att['device'], att['volume_id'], att['attachment_id'])
+                    #    print("    ",
+                    #          vol.attachments[0]['device'],
+                    #          vol.attachments[0]['server_id'],
+                    #          vol.attachments[0]['volume_id'],
+                    #          vol.name[0:20],
+                    #          vol.attachments[0]['id'],
+                    #          vol.cluster_name
+                    #          )
+
                     got_volume = True
+
                 if got_volume and got_image:
                     print("    *** Got both image and attached volumes")
 
